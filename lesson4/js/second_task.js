18c6fe031efb4401ccc812cd3c71095fd089cd17
space-rider <sailorbeavis@gmail.com>
2018-12-04 23:41:04 +0200

    правки
    во втором задании доработан JS код, и другие мелкие правки

--- lesson4/js/second_task.js
+++ lesson4/js/second_task.js
@@ -1,32 +1,40 @@
 let a = +prompt('Введите число');
 
-        document.write('Число : ' + a + '<br />');
+document.write('Число : ' + a + '.<br />');
 
-        function Num() {
-            if (a > 0) {
-                document.write('Число положительное.' + '<br />');
-            }
-            else {
-                document.write('Число отрицательное.' + '<br />');
-            }
-            for(var i = 2; i < a; i++) {
-                if (a % i === 0) {
-                    document.write("Число составное." + "<br>");
-                    break;
-                }
-                else {
-                    if (i == a - 1) {
-                        document.write('Простое число.' + "<br>");
-                    }
-                }
-            }
-            for (i = 2; i <= 9; i++) {
-                if (i == 4 || i == 7 || i == 8) {
-                    continue;
-                }
-                if (a % i === 0) {
-                    document.write("Число делится на: " + i + " без остатка." + "<br>")
-                }
+function Num() {
+    if (a > 0) {
+        document.write('Число положительное.' + '<br />');
+    }
+    else if (a == 0) {
+        document.write('На числовой прямой 0 разделяет положительные и отрицательные числа.' + '<br>');
+        document.write('При делении нуля на любое не нулевое число получается ноль.' + '<br>');
+    }
+    else {
+        document.write('Число отрицательное.' + '<br />');
+    }
+    for (var i = 2; i < a; i++) {
+        if (a % i === 0) {
+            document.write("Число составное." + "<br>");
+            break;
+        }
+        else {
+            if (i == a - 1 || i >= a) {
+                document.write('Простое число.' + "<br>");
             }
         }
- Num();
+    }
+    for (i = 2; i <= 9; i++) {
+        if (a / i == 0) {
+            break;
+        }
+        if (i == 4 || i == 7 || i == 8) {
+            continue;
+        }
+        if (a % i === 0) {
+            document.write("Число делится на: " + i + " без остатка." + "<br>")
+        }
+    }
+}
+
+Num();
