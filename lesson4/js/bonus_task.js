abd69426bf83d354374753f59ce48b0cc24fa468
space-rider <sailorbeavis@gmail.com>
2018-12-05 01:04:59 +0200

    правки
    небольшие правки

--- lesson4/js/bonus_task.js
+++ lesson4/js/bonus_task.js
@@ -1,9 +1,13 @@
-var a = +prompt('Введите целое число : ');
-var b = +prompt('Введите второе целое число : ');
-var c = +prompt('Введите третье целое число : ');
+let a = +prompt('Введите целое число : ');
+let b = +prompt('Введите второе целое число : ');
+let c = +prompt('Введите третье целое число : ');
 
 function Calculate() {
     return (a + b + c) / 3;
 };
+
+document.write('Первое число - ' + a + '<br>');
+document.write('Второе число - ' + b +'<br>');
+document.write('Третье число - ' + c + '<br><br>');
 
 document.write('Среднее арифмитеческое трех чисел с округлением до целого числа : ' + Math.round(Calculate()));
